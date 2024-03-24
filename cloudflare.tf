716cdc3e22c3a3d0237596c961061b50d179bec3
Tyler Evans <snaveevans@users.noreply.github.com>
2024-03-23 22:31:37 -0600

    chore(terraform): add pages domain

--- cloudflare.tf
+++ cloudflare.tf
@@ -15,13 +15,24 @@ variable "CLOUDFLARE_API_TOKEN" {
   type = string
 }
 
+variable "PROJECT_NAME" {
+  type = string
+  default = "civilian-defense-drills"
+}
+
 provider "cloudflare" {
   api_token = var.CLOUDFLARE_API_TOKEN
 }
 
+resource "cloudflare_pages_domain" "my-domain" {
+  account_id   = var.CLOUDFLARE_ACCOUNT_ID
+  project_name = var.PROJECT_NAME
+  domain       = "cdd.tylerevans.co"
+}
+
 resource "cloudflare_pages_project" "civilian-defense-drills" {
   account_id        = var.CLOUDFLARE_ACCOUNT_ID
-  name              = "civilian-defense-drills"
+  name              = var.PROJECT_NAME
   production_branch = "main"
 
   build_config {
