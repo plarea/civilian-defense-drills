2efee226452db18277fc85f25c4d72eb8225e701
Tyler Evans <snaveevans@users.noreply.github.com>
2024-03-25 17:39:15 -0600

    add react-router-dom

---
+++ src/AppRouter.tsx
@@ -0,0 +1,15 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default function AppRouter() {
+  return <RouterProvider router={router} />
+}
